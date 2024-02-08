fc33e14d3b8996b30a7de70a465bde75c2fcf440
juaneforez <juanesforez.95@gmail.com>
2024-02-07 19:53:49 -0500

    Correción de los resources/views
    Los create, edit y index de Course/Learning_Environment/Scheduling_Environment

--- A3/resources/views/scheduling_environment/edit.blade.php
+++ A3/resources/views/scheduling_environment/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.base')
 
-@section('title', 'Editar Actividad')
-@section('header', 'Editar Actividad')
+@section('title', 'Editar programación del ambiente')
+@section('header', 'Editar programación del ambiente')
 
 @section('content')
     @include('templates.messages')
@@ -11,28 +11,45 @@
             <form action="" method="POST">
                 @csrf
                 <div class="row form-group">
-                    <div class="col lg-6 mb-4">
-                        <label for="description">Descripción</label>
-                        <input type="text" class="form-control" id="description" name="description" required>
+                    <div class="col-lg-4 mb-4">    
+                        <label for="course_id">Curso</label>
+                        <select name="course_id" id="course_id"
+                            class="form-control" required>
+                            <option value="">Seleccionar</option>
+                        </select>  
                     </div>
-                    <div class="col lg-6 mb-4">
-                        <label for="hours">Horas estimadas</label>
-                        <input type="number" class="form-control" id="hours" name="hours" required>
+                    <div class="col-lg-4 mb-4">    
+                        <label for="instructor_id">Instructor</label>
+                        <select name="instructor_id" id="instructor_id"
+                            class="form-control" required>
+                            <option value="">Seleccionar</option>
+                        </select>   
+                    </div>
+                    <div class="col-lg-4 mb-4">
+                        <label for="date_scheduling">Fecha de programación</label>
+                        <input type="date" class="form-control"
+                        id="date_scheduling" name="date_scheduling" required>    
                     </div>
                 </div>
                 <div class="row form-group">
-                    <div class="col lg-6 mb-4">
-                        <label for="Technician_id">Tecnico</label>
-                        <select class="form-control" id="Technician_id" name="Technician_id" required>
-                            <option value="">Seleccione</option>
-                        </select>
+                    <div class="col-lg-4 mb-4">
+                        <label for="initial_hour">Hora inicial</label>
+                        <input type="time" class="form-control"
+                        id="initial_hour" name="initial_hour" required>    
                     </div>
-                    <div class="col lg-6 mb-4">
-                        <label for="type_id">Tipo</label>
-                        <select class="form-control" id="type_id" name="type_id" required>
-                            <option value="">Seleccione</option>
-                        </select>
+                    <div class="col-lg-4 mb-4">
+                        <label for="final_hour">Hora final</label>
+                        <input type="time" class="form-control"
+                        id="final_hour" name="final_hour" required>    
                     </div>
+                    <div class="col-lg-4 mb-4">    
+                        <label for="enviroment_id">Ambiente de aprendizaje</label>
+                        <select name="enviroment_id" id="enviroment_id"
+                            class="form-control" required>
+                            <option value="">Seleccionar</option>
+                        </select>      
+                    </div>
+                
                 </div>
                 <div class="row form-group">
                     <div class="col-lg-6 mb-4">
